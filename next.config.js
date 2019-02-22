93a118a7226dfc0c44d5e6d03a3fce03e58d53df
Diego Zunga <diego.zuniga.mendez@gmail.com>
2019-02-21 19:15:02 -0500

    Add next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,32 +1,33 @@
+const withPlugins = require('next-compose-plugins');
 const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const path = require('path');
 
-module.exports = withCss(
-  withSass({
-    webpack(config) {
-      config.resolve.alias['TimelineMax'] = path.join(
-        __dirname,
-        'node_modules/gsap/src/uncompressed/TimelineMax.js',
-      );
-      config.resolve.alias['TweenMax'] = path.join(
-        __dirname,
-        'node_modules/gsap/src/uncompressed/TweenMax.js',
-      );
-      config.resolve.alias['TweenLite'] = path.join(
-        __dirname,
-        'node_modules/gsap/src/uncompressed/TweenLite.js',
-      );
-      config.resolve.alias['ScrollMagic'] = path.join(
-        __dirname,
-        'node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js',
-      );
-      config.resolve.alias['AnimationGsap'] = path.join(
-        __dirname,
-        'node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js',
-      );
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias['TimelineMax'] = path.join(
+      __dirname,
+      'node_modules/gsap/src/uncompressed/TimelineMax.js',
+    );
+    config.resolve.alias['TweenMax'] = path.join(
+      __dirname,
+      'node_modules/gsap/src/uncompressed/TweenMax.js',
+    );
+    config.resolve.alias['TweenLite'] = path.join(
+      __dirname,
+      'node_modules/gsap/src/uncompressed/TweenLite.js',
+    );
+    config.resolve.alias['ScrollMagic'] = path.join(
+      __dirname,
+      'node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js',
+    );
+    config.resolve.alias['AnimationGsap'] = path.join(
+      __dirname,
+      'node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js',
+    );
 
-      return config;
-    },
-  }),
-);
+    return config;
+  },
+};
+
+module.exports = withPlugins([withCss, withSass], nextConfig);
