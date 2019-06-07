10176732ae19c1d0b0d180b79702af5ca0896827
Diego Zunga <diego.zuniga.mendez@gmail.com>
2019-06-06 22:05:05 -0500

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,39 +1,11 @@
 const path = require('path');
-const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 
 const nextConfig = {
   webpack(config, { isServer, dev }) {
-    config.resolve.alias['TimelineMax'] = path.join(
-      __dirname,
-      'node_modules/gsap/src/uncompressed/TimelineMax.js',
-    );
-    config.resolve.alias['TweenMax'] = path.join(
-      __dirname,
-      'node_modules/gsap/src/uncompressed/TweenMax.js',
-    );
-    config.resolve.alias['TweenLite'] = path.join(
-      __dirname,
-      'node_modules/gsap/src/uncompressed/TweenLite.js',
-    );
-    config.resolve.alias['ScrollMagic'] = path.join(
-      __dirname,
-      'node_modules/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js',
-    );
-    config.resolve.alias['AnimationGsap'] = path.join(
-      __dirname,
-      'node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js',
-    );
-
-    config.resolve.alias['@ant-design/icons/lib/dist$'] = path.join(
-      __dirname,
-      'utils/icons.js',
-    );
-
     if (!dev) {
       config.plugins.push(
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         new BundleAnalyzerPlugin({
           analyzerMode: 'static',
           openAnalyzer: false,
@@ -45,7 +17,6 @@ const nextConfig = {
         }),
       );
     }
-
     return config;
   },
 };
